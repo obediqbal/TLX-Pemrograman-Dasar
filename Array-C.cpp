65ca0aa7a84f8e334df84bbff64e85ce6ef28f98
Rezapu Obed <43460093+obediqbal@users.noreply.github.com>
2020-08-05 11:11:23 +0700

    Update Array-C.cpp

--- Array-C.cpp
+++ Array-C.cpp
@@ -1,33 +1,25 @@
 #include <iostream>
-using namespace std;
+
 int main() {
-    int n;
-    cin >> n;
-    if (!(1 <= n && n <= 100000) || cin.fail())return 0;
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-        if (!(1 <= arr[i] && arr[i] <= 1000) || cin.fail())return 0;
+    long n;
+    scanf("%d",&n);
+    int a[n];
+    int count[1001];
+    for(int i=0;i<1001;i++){
+        count[i]=0;
     }
-    int count=0;
-    int max=0;
-    int num=0;
     for(int i=0;i<n;i++){
-        if(arr[i]==0)continue;
-        for(int j=i+1;j<n;j++){
-            if(arr[i]==arr[j]&&arr[j]!=0){
-                count++;
-                arr[j]=0;
-            }
-        }
-        if(count>max){
-            max=count;
-            num=arr[i];
-        }else if(count==max && arr[i]>num){
-            num=arr[i];
-        }
-        count=0;
+        scanf("%d",&a[i]);
+        count[a[i]]++;
     }
-    printf("%i\n",num);
-        return 0;
+    int max=0;
+    int angka;
+    for(int i=1000;i>=0;i--){
+        if(count[i]>max){
+            max=count[i];
+            angka=i;
+        }
+    }
+    printf("%d\n",angka);
+    return 0;
 }
